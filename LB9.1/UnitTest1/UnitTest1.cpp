fa931792d11bed9acd450573a181db0c2e6349b0
anastasiia02-p <anastasiia.prunko.ik.2024@lpnu.ua>
2024-11-05 19:17:20 +0200

    Add files via upload

---
+++ LB9.1/UnitTest1/UnitTest1.cpp
@@ -0,0 +1,28 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../LB9.1/sum.h"
+#include "../LB9.1/var.h"
+#include "../LB9.1/dod.h"
+#include "../LB9.1/sum.cpp"
+#include "../LB9.1/var.cpp"
+#include "../LB9.1/dod.cpp"
+
+#include <cmath>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace nsDod;
+using namespace nsSum;
+using namespace nsVar;
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			
+			Assert::AreEqual(0, nsVar::s, 0.0001);
+		}
+	};
+}
